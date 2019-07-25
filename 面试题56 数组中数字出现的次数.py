a15e3813d7d30fc8480fc4ffeef9b8018a966080
iamzda <40712862+iamzda@users.noreply.github.com>
2019-07-25 09:20:03 +0800

    Create 面试题56 数组中数字出现的次数.py

---
+++ 面试题56 数组中数字出现的次数.py
@@ -0,0 +1,16 @@
+'''
+题目描述
+一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
+'''
+# -*- coding:utf-8 -*-
+class Solution:
+    # 返回[a,b] 其中ab是出现一次的两个数字
+    def FindNumsAppearOnce(self, array):
+        # write code here
+        temp = []
+        for i in array:
+            if i in temp:
+                temp.remove(i)
+            else:
+                temp.append(i)
+        return temp
